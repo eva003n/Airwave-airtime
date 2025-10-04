780e44b4e41b61a6b78829d53280037c29903ed2
Evan ngugi wambui <evanngugi547@gmail.com>
2025-10-04 22:02:54 +0300

    add changes

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -14,7 +14,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 
 // Import config (make sure config/config.json or config/config.ts exists)
-import configFile from "../config/config.json" with { type: "json" };
+import configFile from "../config/database/config.json" with { type: "json" };
 const config = (configFile as any)[env];
 
 interface DB {
--- backend/src/routes/wallet.routes.ts
+++ backend/src/routes/wallet.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import { getWalletBalance } from "../controllers/wallet.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = Router()
+router.use(protectRoute)
 
-router.route("/balance")
+router.route("/balance").get(getWalletBalance)
 router.route("/deposit")
 
 
