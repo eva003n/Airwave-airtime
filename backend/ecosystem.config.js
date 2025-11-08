0e4c5c1b810aa38a889021d30911936263f50590
Evan ngugi wambui <evanngugi547@gmail.com>
2025-11-08 14:43:43 +0300

    add changes

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,18 +1,18 @@
 export const apps = [
   // Process 1: Main HTTP API
-  {
-    name: "airwave-airtime-sandbox-api",
-    // script: "./index.js", //docker
-    script: "dist/index.js", //local
-    instances: 1,
-    autorestart: true,
-    // watch: ["."],
-    env: {
-      NODE_ENV: "development",
-      PORT: 8000,
-      PM2_NO_PIDUSAGE: "true", // ✅ disables wmic
-    },
-  },
+  // {
+  //   name: "airwave-airtime-sandbox-api",
+  //   // script: "./index.js", //docker
+  //   script: "dist/index.js", //local
+  //   instances: 1,
+  //   autorestart: true,
+  //   // watch: ["."],
+  //   env: {
+  //     NODE_ENV: "development",
+  //     PORT: 8000,
+  //     PM2_NO_PIDUSAGE: "true", // ✅ disables wmic
+  //   },
+  // },
 
   // Process 2: Worker for topups
   {
@@ -22,25 +22,25 @@ export const apps = [
     instances: 1,
     autorestart: true,
     env: {
-      // NODE_ENV: "development",
+      NODE_ENV: "production",
       PORT: 8100,
       PM2_NO_PIDUSAGE: "true", // ✅ disables wmic
     },
   },
 
   // Production service
-  // {
-  //   name: "airwave-airtime-api",
-  //   // script: "./workers/topup.worker.js", //docker
-  //   script: "dist/index.js",
-  //   instances: 1,
-  //   autorestart: true,
-  //   env: {
-  //     NODE_ENV: "production",
-  //     PORT: 8080,
-  //     PM2_NO_PIDUSAGE: "true", // ✅ disables wmic
-  //   },
-  // },
+  {
+    name: "airwave-airtime-api",
+    // script: "./workers/topup.worker.js", //docker
+    script: "dist/index.js",
+    instances: 1,
+    autorestart: true,
+    env: {
+      NODE_ENV: "production",
+      PORT: 8080,
+      PM2_NO_PIDUSAGE: "true", // ✅ disables wmic
+    },
+  },
 ];
 export const deploy = {
   production: {
