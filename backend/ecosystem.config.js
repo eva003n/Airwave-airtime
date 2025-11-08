07cd7c5ca43d91eabf17138fd004314f32631c04
Evan ngugi wambui <evanngugi547@gmail.com>
2025-11-08 07:32:05 +0300

    update pm2 config file

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,17 +1,17 @@
 export const apps = [
   // Process 1: Main HTTP API
-  {
-    name: "airwave-airtime-sandbox-api",
-    // script: "./index.js", //docker
-    script: "dist/index.js", //local
+  // {
+  //   name: "airwave-airtime-sandbox-api",
+  //   // script: "./index.js", //docker
+  //   script: "dist/index.js", //local
   
-    // watch: ["."],
-    env: {
-      NODE_ENV: "development",
-      PORT: 8000,
-      PM2_NO_PIDUSAGE: "true", // ✅ disables wmic
-    },
-  },
+  //   // watch: ["."],
+  //   env: {
+  //     NODE_ENV: "development",
+  //     PORT: 8000,
+  //     PM2_NO_PIDUSAGE: "true", // ✅ disables wmic
+  //   },
+  // },
 
   // Process 2: Worker for topups
   // {
