23096029e01ce953934822af41d4ed746bb2761a
Evan ngugi wambui <evanngugi547@gmail.com>
2026-02-25 22:08:23 +0300

    add change

--- backend/src/routes/recipient.routes.ts
+++ backend/src/routes/recipient.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createRecipient, createRecipients, deleteRecipient, getAllrecipients, getRecipient, updateRecipient } from "../controllers/recipient.controller.js";
+import { createRecipient, createRecipients, deleteRecipient, getAllRecipients, getRecipient, updateRecipient } from "../controllers/recipient.controller.js";
 import { IdSchema, multipleRecipientSchema, paginateSchema, recipientQuerySchema, recipientSchema } from "../middlewares/validators/validators.js";
 import { validate } from "../middlewares/validators/validator.middleware.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
@@ -18,7 +18,7 @@ router.route("/:id").delete(validate(IdSchema), deleteRecipient)
 //create recipients in bulk
 router.route("/bulk").post(validate(multipleRecipientSchema), createRecipients)
 //get all airtime recipients
-router.route("/").get(validate(recipientQuerySchema), getAllrecipients)
+router.route("/").get(validate(recipientQuerySchema), getAllRecipients)
 router.route("/:id").get(validate(IdSchema), getRecipient)
 
 export default router;
